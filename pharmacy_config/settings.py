a9ece783cc26b37829879a2282f4541169f0ecfd
joseph-nextgenwebdeveloper <joseph.nextgenweb.dev@gmail.com>
2026-09-07 16:03:58 +0300

    add allowed hosts

--- pharmacy_config/settings.py
+++ pharmacy_config/settings.py
@@ -25,7 +25,6 @@ ALLOWED_HOSTS = [
     for host in os.getenv(
         "ALLOWED_HOSTS",
         "127.0.0.1,localhost"
-        "https://pharmacy-stock-app-backend.onrender.com/"
     ).split(",")
 ]
 
